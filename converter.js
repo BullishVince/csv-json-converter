0f6436f47d97dd8233c40ab22d0fadfcd675badf
BullishVince <vincent@bullishlife.com>
2021-12-22 23:38:49 +0100

    update convertToJson function (WIP)

--- converter.js
+++ converter.js
@@ -1,8 +1,29 @@
+const fs = require('fs');
+_ = require('underscore');
 
+const convertToJson = input => {
+    fs.readFile(input, 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        const lines = data.split('\n')
+        const result = []
+        const headers = lines[0].split(',')
 
+        lines.map(l => {
+            const obj = {}
+            const line = l.split(',')
 
-function convertToJson(input) {
-    return 'Hello World';
+            headers.map((h, i) => {
+                obj[h] = line[i]
+            })
+
+            result.push(obj)
+        })
+        console.log(result);
+        return result;
+      })
 }
 
 exports.convertToJson = convertToJson;
--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
-var argv = require('yargs/yargs')(process.argv.slice(2))
+const argv = require('yargs/yargs')(process.argv.slice(2))
     .usage('Usage: \n-i [path to the input file which you want to convert]\n-o [filepath used to store the output]')
     .demandOption(['i','o'])
     .argv;
+const fs = require('fs');
 converter = require('./converter');
 
 console.log(`Input: ${argv.i} , Output: ${argv.o}`);
 console.log('Running application...');
 
-console.log(converter.convertToJson());
+if (String(argv.i).includes('.csv')) {
+    json = converter.convertToJson(argv.i);
+    fs.writeFile(argv.o, json, err => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`Saved result to ${argv.o}`);
+      })
+} else {
+    console.log('Converting JSON to CSV');
+}
